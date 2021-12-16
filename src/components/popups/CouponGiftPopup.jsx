16499b2d884c20ebb866e805863e3daeaecf59d8
Mohammed_Alghazali <malghazali@itgsoftware.com>
2021-12-16 18:35:31 +0200

    add styles for the /mypage/coupon/coupon-gift popup

--- src/components/popups/CouponGiftPopup.jsx
+++ src/components/popups/CouponGiftPopup.jsx
@@ -7,43 +7,43 @@ import './Scrollbars.css'
 
 const CouponGiftPopup = ({setPopupOpen}) => {
     return (
-        <div className="h-731px w-610px flex flex-col rounded-lg overflow-hidden">
-            <div className="relative h-71px bg-blue-r5d799c flex items-center justify-center flex-shrink-0">
-                <label className="font-spoqaBold text-white tracking-tight text-24px">쿠폰 선물하기</label>
-                <button className="absolute right-0 mr-26px cursor-pointer z-20" onClick={()=> setPopupOpen(false)}>
-                    <img src={CloseIcon} alt="close_icon" />
+        <div style={{height:"100vh", width: '77.625rem' ,borderTopLeftRadius: '4rem', borderTopRightRadius: '4rem'}} className="flex flex-col rounded-lg overflow-hidden">
+            <div style={{height: '9.5rem'}} className="relative bg-blue-r5d799c flex items-center justify-center flex-shrink-0">
+                <label style={{fontSize: '3.75rem'}} className="font-spoqaBold text-white tracking-tight">베팅내역</label>
+                <button style={{marginRight: '4.625rem'}} className="absolute right-0 cursor-pointer z-20" onClick={() => setPopupOpen(false)}>
+                    <img style={{width: '3.1875rem', height: '3.1875rem'}} src={CloseIcon} alt="close_icon" />
                 </button>
             </div>
 
-            <div className="w-full h-full bg-white pl-33px pr-37px">
-                <div className="flex items-center h-19px mt-36px space-x-8px">
-                    <img src={Step1} alt="" />
-                    <span className="font-spoqaMedium text-16px tracking-tight text-gray-r7c7c7c pt-3px">선물로 보낼 쿠폰을 확인하세요.</span>
+            <div style={{padding: '0 1.875rem'}} className="w-full h-full bg-white">
+                <div style={{marginTop: '3.125rem', marginBottom: '1.875rem'}} className="flex items-start">
+                    <img style={{width: '7.9375rem', height: '2.875rem', marginLeft: '0.4375rem', marginRight: '1.875rem'}} src={Step1} alt="" />
+                    <span style={{fontSize: '2.625rem'}} className="font-spoqaMedium tracking-tight text-gray-r7c7c7c">선물로 보낼 쿠폰을 확인하세요.</span>
                 </div>
 
-                <div className="mt-17px h-64px w-full bg-gray-f9f9f9 border border-gray-dddddd rounded-2xl flex items-center font-spoqaMedium text-14px tracking-tight ">
-                    <span className="ml-76px text-gray-r454545">선물할 쿠폰</span>
-                    <div className="w-311px h-42px ml-10px bg-white border border-gray-dddddd rounded-md flex items-center">
-                        <span className="text-gray-r8c8c8c ml-11px">쿠폰테스트</span>
+                <div style={{height: '10.4375rem', fontSize: '2.8125rem', marginBottom: '7.5rem'}} className="w-full bg-gray-f9f9f9 border border-gray-dddddd rounded-2xl flex items-center justify-center font-spoqaMedium tracking-tight ">
+                    <span className="text-gray-r454545">선물할 쿠폰</span>
+                    <div style={{height: '6.75rem', width: '41.4375rem', marginLeft: '1.875rem'}} className="bg-white border border-gray-dddddd rounded-md flex items-center">
+                        <span style={{paddingLeft: '1.5625rem'}} className="text-gray-r8c8c8c">쿠폰테스트</span>
                     </div>
                 </div>
 
-                <div className="flex items-center h-19px mt-40px space-x-8px">
-                    <img src={Step2} alt="" />
-                    <span className="font-spoqaMedium text-16px tracking-tight text-gray-r7c7c7c pt-3px">쿠폰받을 회원을 선택하세요.</span>
+                <div style={{marginBottom: '1.875rem'}} className="flex items-center">
+                    <img  style={{width: '7.9375rem', height: '2.875rem', marginLeft: '0.4375rem', marginRight: '1.875rem'}} src={Step2} alt="" />
+                    <span style={{fontSize: '2.625rem'}} className="font-spoqaMedium tracking-tight text-gray-r7c7c7c">쿠폰받을 회원을 선택하세요.</span>
                 </div>
 
-                <div style={{borderRadius:"0.5rem"}} className="mt-17px w-full h-337px rounded-2xl shadow-subNavbar overflow-hidden">
+                <div style={{borderRadius:"0.5rem"}} className="w-full rounded-2xl shadow-subNavbar overflow-hidden">
                     <CouponGiftPopupTable />
                 </div>
 
-                <div className="w-full flex items-center justify-center mt-21px">
-                    <button className="flex items-center justify-center h-52px w-192px rounded-lg bg-blue-r0070d9" onClick={()=> setPopupOpen(false)}>
+                <div className="w-full flex items-center justify-center">
+                    <button style={{height: '7.3125rem', width: '36.4375rem', padding: '0.1875rem', fontSize: '2.8125rem', marginTop: '3.75rem'}} className="flex items-center justify-center rounded-lg bg-blue-r0070d9" onClick={()=> setPopupOpen(false)}>
                         <div 
                         style={{background:"linear-gradient(to bottom, #1491fc, #0675db)"}} 
-                        className="flex items-center justify-center h-50px w-190px rounded-lg border border-blue-r3ba3fc bg-gradient-to-b from-blue-r1491fc via-blue-r1491fc to-blue-r0675db cursor-pointer"
+                        className="flex w-full h-full items-center justify-center rounded-lg border border-blue-r3ba3fc bg-gradient-to-b from-blue-r1491fc via-blue-r1491fc to-blue-r0675db cursor-pointer"
                         >
-                            <span className="font-spoqaMedium tracking-tight text-16px text-white">쿠폰발송</span>
+                            <span className="font-spoqaMedium tracking-tight text-white">쿠폰발송</span>
                         </div>
                     </button>
                 </div>
