817115d2e5e1a91fdb983654e89d03fcb072deac
Mohammed_Alghazali <malghazali@itgsoftware.com>
2021-12-20 00:38:08 +0200

    add the general layout for the /cscenter/policy/sportsgame/soccer pages

--- src/components/cscenter/tables/SportsGamePolicyTable2.jsx
+++ src/components/cscenter/tables/SportsGamePolicyTable2.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 const SportsGamePolicyTable2 = ({array}) => {
 
     function Cells({ items }) {     
-        return items.map(item => (
-            <tr className="bg-gray-fefefe font-spoqa text-14px tracking-tight text-gray-r585858 h-56px w-full border-b border-gray-dddddd">
-                <td style={{width: "248px"}} className="font-spoqaMedium h-56px text-center pl-20px flex justify-around items-center">
-                    <p style={{color: item.color}}>{item.title}</p>
+        return items.map((item, index) => (
+            <tr style={{borderBottomWidth: `${items.length - 1 !== index? "0.1875rem" : ""}`}} className="bg-gray-fefefe font-spoqa tracking-tight text-gray-r585858 w-full border-b border-gray-dddddd">
+                <td style={{padding: '0.875rem 0', width: "29.375rem", paddingLeft: '2.5rem', paddingRight: '1.5rem'}} className="font-spoqaMedium  text-center">
+                    <span style={{color: item.color}}>{item.title}</span>
                     {item.title2 && (
-                        <p>{item.title2}</p>
+                        <span style={{marginLeft: '5.3125rem'}}>{item.title2}</span>
                     )}
                 </td>
-                <td style={{width: "792px", WebkitTextStroke:"0.2px"}} className="h-56px pl-20px -space-y-4px">
+                <td style={{padding: '0.875rem 0', WebkitTextStroke:"0.2px"}} className="">
                     <p>{item.text}</p>
                     <p>{item.text2}</p>
                 </td>
@@ -21,14 +21,14 @@ const SportsGamePolicyTable2 = ({array}) => {
 
     
     return (
-        <table style={{borderRadius:"1em"}} className="shadow-subNavbar overflow-hidden">
-            <thead className="bg-gray-fafafa font-spoqaMedium text-14px tracking-tight text-gray-r454545 h-56px border-b border-gray-dddddd">
-                <tr>
-                    <td style={{width: "248px"}} className="text-center pl-20px">타입및 경기시간</td>
-                    <td style={{width: "792px"}} className="text-center">결과적용기준</td>
+        <table style={{borderRadius:"1em",margin: '0 1.875rem', marginTop: '2.5rem' ,fontSize: '2.8125rem'}} className="shadow-subNavbar overflow-hidden">
+            <thead style={{borderBottomWidth:  "0.1875rem"}} className="bg-gray-fafafa font-spoqaMedium tracking-tight text-gray-r454545  border-b border-gray-dddddd">
+                <tr style={{height: '9.375rem'}}>
+                    <td style={{width: "29.375rem"}} className="text-center">타입및 경기시간</td>
+                    <td className="text-center">결과적용기준</td>
                 </tr>
             </thead>
-            <tbody className="w-full text-585858 text-14px tracking-tight font-spoqa">
+            <tbody style={{fontSize: '2.625rem'}} className="w-full text-585858 tracking-tight font-spoqa">
                 <Cells items={array} />
             </tbody>
         </table>     
---
+++ src/components/horizontalMenus/HorizontalSubMenu1.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+import { useHistory } from 'react-router';
+
+
+const HorizontalSubMenu1 = ({
+    itemsArray, 
+    setSelectedTab,
+    setSelectedSubTab = null
+}) => {
+
+    const history = useHistory();
+    const pathname = window.location.pathname
+    const [isHover, setHover] = useState(null)
+    console.log(`isHover`, isHover)
+
+    function TabsList({ items }) {
+        return items.map((item, index) => (
+            <button 
+            className={`${
+                pathname === item.path
+                ? "text-black" 
+                : "text-gray-b7b7b7"
+            } rounded-full min-w-fit bg-white text-black flex items-center`}
+            style={{height: '6.375rem', minWidth: 'fit-content' , fontSize: '2.8125rem', marginRight: `${!index ? '0.9375rem' : '' }`}} 
+            key={item.id} 
+            onClick={() => {
+                history.push(item.path)
+                setSelectedTab(item.id)
+                if (setSelectedSubTab !== null) {
+                    setSelectedSubTab(0)
+                }
+            }}
+            onMouseOver={() => setHover(item.id)}
+            onMouseLeave={() => setHover(null)}
+            >
+                <div style={{margin: '0 3.39rem'}} >
+                    <div>
+                        <span
+                            className={`${
+                                pathname === item.path
+                                ? "text-black" 
+                                : "text-gray-b7b7b7 hover:text-black"
+                            }`}
+                        >{item.text}</span>
+                    </div>
+                </div>
+                <div style={{height: '2.0625rem', width: '0.1875rem', margin: '', marginTop: ''}} className='bg-gray-c5c5c5' />
+            </button>
+        ));
+    }
+
+    return (
+        <div id="container" className="flex justify-start items-start">
+            <TabsList items={itemsArray} />
+        </div>
+    )
+}
+
+export default HorizontalSubMenu1
