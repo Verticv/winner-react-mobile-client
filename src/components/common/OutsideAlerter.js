8916748305538b1dd40bb57a88467321fba27274
Mohammed_Alghazali <malghazali@itgsoftware.com>
2022-01-21 10:35:58 +0200

    add dependency

--- src/components/common/OutsideAlerter.js
+++ src/components/common/OutsideAlerter.js
@@ -20,7 +20,7 @@ function useOutsideAlerter(ref, setDropdownOpen) {
             // Unbind the event listener on clean up
             document.removeEventListener("mousedown", handleClickOutside);
         };
-    }, [ref]);
+    }, [ref, setDropdownOpen]);
 }
 
 /**
