c8045fd2232a9634709466f1444641c8d533705e
Mohammed_Alghazali <malghazali@itgsoftware.com>
2022-02-02 00:45:12 +0200

    try a solution

--- src/utils/horizontalsScroll.js
+++ src/utils/horizontalsScroll.js
@@ -6,8 +6,23 @@ export default function horizontalsScroll(array, tapIdPrefix, scrollWrapperId, b
     if (activeTapId > 2) {
       const elementWidth = window.document.querySelector(`#${tapIdPrefix}${activeTapId}`)?.offsetWidth;
       const leftScrollValue = ((activeTapId - 1.5) * elementWidth)
-      console.log(leftScrollValue)
-      scrollWrapper.scrollLeft = Number(leftScrollValue);
+      if ('scroll-behavior' in document.documentElement.style) {
+        scrollWrapper.scrollLeft = Number(leftScrollValue);
+      } else {
+        let leftScrollValueAmount = Number(leftScrollValue);
+        console.log('leftScrollValueAmount', leftScrollValueAmount);
+        const myInterval = setInterval(() => {
+          const scrolledValue = leftScrollValueAmount / 2
+          console.log('scrolledValue', scrolledValue);
+          scrollWrapper.scrollLeft += scrolledValue
+          leftScrollValueAmount = leftScrollValueAmount - scrolledValue
+          if (leftScrollValueAmount < 1) {
+            console.log('inside if?!', leftScrollValueAmount);
+            clearInterval(myInterval);
+          }
+        }, 300);
+      }
+
     } else {
       scrollWrapper.scrollLeft = 0;
     }
