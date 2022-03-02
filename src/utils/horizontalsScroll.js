8461fcfc2b7a6d5f2a74cd48365f3fc55cbb0a67
Mohammed_Alghazali <malghazali@itgsoftware.com>
2022-03-03 00:29:21 +0200

    Fix scroll behavior when scroll back

--- src/utils/horizontalsScroll.js
+++ src/utils/horizontalsScroll.js
@@ -2,6 +2,9 @@ export default function horizontalsScroll(array, tapIdPrefix, scrollWrapperId, b
   const currentPath = window.location.pathname;
   const activeTapId = buttonIndex || array?.filter((item) => (item.path === currentPath))?.[0]?.id || array?.filter((item) => (item.subPath === currentPath))?.[0]?.id || array?.filter((item) => (item.subPath2 === currentPath))?.[0]?.id || array?.filter((item) => (item.subPath3 === currentPath))?.[0]?.id || array?.filter((item) => (item.subPath4 === currentPath))?.[0]?.id
   const scrollWrapper = window.document.querySelector(`#${scrollWrapperId}`);
+  if (!window.prevActiveTapId) {
+    window.prevActiveTapId = activeTapId
+  }
   if (scrollWrapper) {
     if (activeTapId > 2) {
       const elementWidth = window.document.querySelector(`#${tapIdPrefix}${activeTapId}`)?.offsetWidth;
@@ -12,9 +15,14 @@ export default function horizontalsScroll(array, tapIdPrefix, scrollWrapperId, b
         let leftScrollValueAmount = Number(leftScrollValue);
         const myInterval = setInterval(() => {
           const scrolledValue = leftScrollValueAmount / 4
-          scrollWrapper.scrollLeft += scrolledValue
+          if (window.prevActiveTapId < activeTapId || window.prevActiveTapId === activeTapId) {
+            scrollWrapper.scrollLeft += scrolledValue
+          } else {
+            scrollWrapper.scrollLeft -= scrolledValue
+          }
           leftScrollValueAmount = leftScrollValueAmount - scrolledValue
           if (leftScrollValueAmount < 1) {
+            window.prevActiveTapId = activeTapId
             clearInterval(myInterval);
           }
         }, 16);
